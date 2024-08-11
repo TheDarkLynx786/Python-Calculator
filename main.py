bf9387176dd3efb81b2e0866777f2c064652fcea
Murtaza Haque <MurtazaHaque@my.unt.edu>
2024-08-11 12:40:46 -0500

    Numpad Fully Functional
    All the buttons except the equal sign works. Next step is to add the operators.

--- main.py
+++ main.py
@@ -4,33 +4,66 @@ import math
 
 #Configure Window 1
 window = tkinter.Tk()
-window.title("Input Window")
+window.title("Python Calculator")
 window.configure(bg='#002421')
 
+displayBuffer = ""
+display = tkinter.Label(window, text="", bg='#424242', fg='#FFFFFF', padx=3, pady=3, width="20")
+display.grid(row = 0, column = 0, columnspan=3)
+
+def displayHandler(elem):
+    global displayBuffer
+    txt = elem.cget("text")
+    
+    if txt == "<":
+        displayBuffer = displayBuffer[:-1]
+    elif txt == "+/-":
+        if not "-" in displayBuffer:
+            displayBuffer = "-" + displayBuffer
+        else:
+            displayBuffer = displayBuffer[1:]
+    elif txt == ".":
+        if not "." in displayBuffer:
+            displayBuffer += txt
+    else:
+        displayBuffer += txt
+
+    global display
+    display = tkinter.Label(window, text=displayBuffer, bg='#424242', fg='#FFFFFF', padx=3, pady=3, width="20")
+    display.grid(row = 0, column = 0, columnspan=3)
+
+
 def renderElems(*render):    
-    #Render Elements 
+    #Render Number Buttons 
     for i in range(len(render)):      
         for l in range(3):
             if(i*3 + l >= len(render)):
                 break
-            render[(i*3 + l)].grid(row=i, column=l, padx=3,pady=3)
+            render[(i*3 + l)].grid(row=i+1, column=l, padx=3,pady=3)
 
-#Buttons
-b1 = tkinter.Button(window, text="1")
-b2 = tkinter.Button(window, text="2")
-b3 = tkinter.Button(window, text="3")
-b4 = tkinter.Button(window, text="4")
-b5 = tkinter.Button(window, text="5")
-b6 = tkinter.Button(window, text="6")
-b7 = tkinter.Button(window, text="7")
-b8 = tkinter.Button(window, text="8")
-b9 = tkinter.Button(window, text="9")
-b10 = tkinter.Button(window, text="0")
-b11 = tkinter.Button(window, text="+/-")
-b12 = tkinter.Button(window, text=".")
-equals = tkinter.Button(window, text="=")
 
-renderElems(b1,b2,b3,b4,b5,b6,b7,b8,b9,b10,b11,b12,equals)
+
+#Number Buttons
+btnWdth = 6
+b1 = tkinter.Button(window, text="1", width = btnWdth, command=lambda: [displayHandler(b1)])
+b2 = tkinter.Button(window, text="2", width = btnWdth, command=lambda: [displayHandler(b2)])
+b3 = tkinter.Button(window, text="3", width = btnWdth, command=lambda: [displayHandler(b3)])
+b4 = tkinter.Button(window, text="4", width = btnWdth, command=lambda: [displayHandler(b4)])
+b5 = tkinter.Button(window, text="5", width = btnWdth, command=lambda: [displayHandler(b5)])
+b6 = tkinter.Button(window, text="6", width = btnWdth, command=lambda: [displayHandler(b6)])
+b7 = tkinter.Button(window, text="7", width = btnWdth, command=lambda: [displayHandler(b7)])
+b8 = tkinter.Button(window, text="8", width = btnWdth, command=lambda: [displayHandler(b8)])
+b9 = tkinter.Button(window, text="9", width = btnWdth, command=lambda: [displayHandler(b9)])
+b10 = tkinter.Button(window, text="+/-", width = btnWdth, command=lambda: [displayHandler(b10)])
+b11 = tkinter.Button(window, text="0", width = btnWdth, command=lambda: [displayHandler(b11)])
+b12 = tkinter.Button(window, text=".", width = btnWdth, command=lambda: [displayHandler(b12)])
+
+#Operators and Functions
+
+equals = tkinter.Button(window, width = btnWdth, text="=")
+delete = tkinter.Button(window, width = btnWdth, text="<", command=lambda: [displayHandler(delete)])
+
+renderElems(b1,b2,b3,b4,b5,b6,b7,b8,b9,b10,b11,b12,equals,delete)
 
 
 
